9bb7d697127bca5fab66d72f7411883c8e292f2c
Maksym Motoman <maxonlinux@users.noreply.github.com>
2025-12-30 02:56:08 -0800

    WIP

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.PROXY_URL}/:path*`,
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
